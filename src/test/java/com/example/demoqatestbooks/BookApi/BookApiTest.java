464bd152e5d4b90fb68dcb7bb4d57673850a0d39
stepaandr013 <stepa.andr@gmail.com>
2022-08-24 19:09:00 +0300

    apiTest_V2

--- src/test/java/com/example/demoqatestbooks/BookApi/BookApiTest.java
+++ src/test/java/com/example/demoqatestbooks/BookApi/BookApiTest.java
@@ -1,7 +1,12 @@
 package com.example.demoqatestbooks.BookApi;
 
 import com.example.demoqatestbooks.readConfig.ConfigProvider;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
 
 public class BookApiTest {
     private final static String URL = ConfigProvider.URL;
@@ -9,6 +14,14 @@ public class BookApiTest {
     @Test
     public void checkBooks(){
         Specifications.installSpecification(Specifications.requestSpecification(URL), Specifications.responseSpecification(200));
+        List<Books> books = given()
+                .when()
+                .get("BookStore/v1/Books")
+                .then().log().all()
+                .extract().body().jsonPath().getList("books", Books.class);
+
+        books.forEach(x-> Assert.assertFalse(x.getTitle().isEmpty()));
+        books.forEach(x-> Assert.assertTrue(x.getPages() > 200));
     }
 
 }
